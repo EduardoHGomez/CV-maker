c8fb9ff2dfd1ca507a40bd2bb8e94afa44bb4107
Eduardo <hurtado.josegomez@gmail.com>
2023-10-11 23:07:20 -0600

    Details div

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,14 @@
 // Personal info details
 
+
+// Main component of details
 function Details(){
     return (
         <div>
             <h1>Personal info</h1>
+            <form className="detailsInput">
+                <input className="name"></input>
+            </form>
         </div>
     )
 }

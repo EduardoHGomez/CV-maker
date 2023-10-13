1445ae21cd63c782e540acade705f2645b9e0efb
Eduardo <hurtado.josegomez@gmail.com>
2023-10-12 22:10:50 -0600

    Filling first experience fields

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,8 +3,18 @@
 function Experience() {
     return (
         <div className="experienceContainer">
-        
-
+            <label>
+                Company name:
+                <input className="experienceCompany"></input>
+            </label>
+            <label>
+                Position:
+                <input className="experiencePositon"></input>
+            </label>
+            <label>
+                Title:
+                <input className="experienceTitle"></input>
+            </label>
         </div>
     )
 

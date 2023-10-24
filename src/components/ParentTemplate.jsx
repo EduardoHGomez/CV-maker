b79ae7eec965124356e4d1c2e156e81d3a34b1d7
Eduardo <hurtado.josegomez@gmail.com>
2023-10-23 23:02:29 -0600

    inputs added

--- src/components/ParentTemplate.jsx
+++ src/components/ParentTemplate.jsx
@@ -6,11 +6,13 @@ import Education from './components/Education'
 // This file was made for testing purposes: view the effect of an input to a cv template
 
 function MainContainer() {
+    const [textInput, setTextInput] = useState('');
+
     return (
         <div>
+            <h1>New Container</h1>
+            <
 
         </div>
-
-
     );
 }

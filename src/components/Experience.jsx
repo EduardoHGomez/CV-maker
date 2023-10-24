2460a0dbfd496a771a4c785a92f83f9c00014155
Eduardo <hurtado.josegomez@gmail.com>
2023-10-23 23:12:58 -0600

    First props being used

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import {useState} from "react";
 
 function Experience() {
     const [companyInput, setCompanyInput] = useState('Company 1');
--- src/components/ParentTemplate.jsx
+++ src/components/ParentTemplate.jsx
@@ -4,12 +4,30 @@ import ReactDOM from 'react-dom/client'
 import Experience from './Experience'
 // This file was made for testing purposes: view the effect of an input to a cv template
 
+function ContainerTest(props) {
+    return (
+        <div>
+        <label>
+            Writing on the next container:
+            <input type="text">
+
+            </input>
+        </label>
+
+
+        </div>
+
+    );
+}
+
 function ParentTemplate() {
     const [textInput, setTextInput] = useState('');
 
     return (
         <div>
             <h1>New Container</h1>
+            <Experience/>
+            <ContainerTest/>
 
         </div>
     );

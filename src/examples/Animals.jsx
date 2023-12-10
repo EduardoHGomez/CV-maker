bef130a2d158320195842d4e3495826f7ecbf324
EduardoHGomez <hurtado.josegomez@gmail.com>
2023-12-09 22:11:45 -0600

    Ternary and ListItems

--- src/examples/Animals.jsx
+++ src/examples/Animals.jsx
@@ -1,16 +1,28 @@
 
+function ListItem(props) {
+    return (
+        <li key={props.animal}>{props.animal}</li>
+    );
+}
+
+function List(props) {
+    return (
+        <ul>
+            {props.animals.map((animal) => {
+                return animal.startsWith("L") ? <ListItem key={animal} animal={animal}/> : null;
+            })}
+        </ul>
+    );
+}
+
 function Animals() {
-    const animals = ["Lion", "Lizard", "Dog", "Cat"];
+    const animals = ["Lion", "Lizard", "Cat", "Dog"];
 
     return (
         <div>
         <h1>
             Animals: </h1>
-            <ul>
-                {animals.map((animal) => {
-                    return <li key={animal}>{animal}</li>
-                })}
-            </ul>
+            <List animals={animals}></List>
         </div>
     );
 }

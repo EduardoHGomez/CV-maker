af7c2c05476a8d06be9aa59008f8a8c36c48f5fc
EduardoHGomez <hurtado.josegomez@gmail.com>
2023-12-10 00:33:07 -0600

    Overriding Functions

--- src/examples/App1.jsx
+++ src/examples/App1.jsx
@@ -1,22 +1,40 @@
 
-function Button({text, color, fontSize}) {
+function Button({text, color, fontSize, handleClick}) {
     const buttonStyle = {
         color: color,
         fontSize: fontSize + 'px'
     }
 
     return (
-        <button style={buttonStyle}>{text}</button>
+        <button onClick={() => handleClick('https://www.theodinproject.com')} style={buttonStyle}>{text}</button>
     );
 }
 
+Button.defaultProps = {
+    text: "Default text",
+    color: "brown",
+    fontSize: 12
+};
+
 
 export default function App1() {
+
+    const handleButtonClick = (url) => {
+        window.location.href = url;
+    };
+
     return (
         <>
             <Button text="Click Me!" color="blue" fontSize={12}></Button>
             <Button text="Don't Click Me!" color="red" fontSize={14}></Button>
             <Button text="Click Me!" color="green" fontSize={16}></Button>
+            {/* Default props */}
+            <Button></Button>
+
+            {/* Props as functions */}
+            <Button handleClick={() => handleButtonClick('http://www.google.com')} color="yellow"></Button>
+            <Button handleClick={handleButtonClick} color="green"></Button>
+
         </>
     );
 }

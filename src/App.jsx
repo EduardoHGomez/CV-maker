4d4df599bb751d93547c8bccf353cb72b27ba6f4
EduardoHGomez <hurtado.josegomez@gmail.com>
2023-12-11 17:57:42 -0600

    Initial data passed to BasicInfoView

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import BasicInfoView from './components/views/BasicInfoView'
 import BasicInfoEditor from './components/editors/BasicInfoEditor'
 
 function App() {
+	const data = {
+		name: 'John',
+		email: 'john@email.com',
+		phone: '33-0000-1111'
+	}
 
 	return (
 		<div className="container">
 			<BasicInfoEditor/>
+			<BasicInfoView data={data}/>
 		</div>
 	);
 }
--- src/components/views/BasicInfoView.jsx
+++ src/components/views/BasicInfoView.jsx
@@ -2,10 +2,14 @@
 import React, {useState} from "react";
 
 // Main component of details
-function BasicInfoView(){
+function BasicInfoView({data}){
     return (
-
-        <h1>View</h1>
+        <div className="rightColumn">
+            <h1>Basic information</h1>
+            <p><b>Name: </b>{data.name}</p>
+            <p><b>Email: </b>{data.email}</p>
+            <p><b>Phone: </b>{data.phone}</p>
+        </div>
     );
 }
 

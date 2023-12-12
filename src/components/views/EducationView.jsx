70fb0da0295815b19b58cb6dcc925697402b5c7b
EduardoHGomez <hurtado.josegomez@gmail.com>
2023-12-12 09:19:40 -0600

    Views created

---
+++ src/components/views/EducationView.jsx
@@ -0,0 +1,13 @@
+// Personal info details
+import React, {useState} from "react";
+
+function Education() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Education;
---
+++ src/components/views/ExperienceView.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useState} from "react";
+
+function Experience() {
+
+
+    return (
+        <div className="experienceContainer">
+
+        </div>
+    )
+
+}
+
+export default Experience;
